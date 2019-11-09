bc233e144501baf5c204491753cf0267bbc687e2
everett-lee <everett.lee@btinternet.com>
2019-11-09 20:51:35 +0000

    add algo tests

--- src/components/rowelimination/RREFAlgorithm.js
+++ src/components/rowelimination/RREFAlgorithm.js
@@ -1,20 +1,17 @@
-//import copyMatrix from '../utils/CopyMatrix';
-
-const copyMatrix = require("../utils/CopyMatrix.js");
-
+import copyMatrix from '../utils/CopyMatrix';
 
 /**
  * Algorithm to convert input matrix to reduced row echelon form. Based on
  * pseudocode from https://rosettacode.org/wiki/Reduced_row_echelon_form
  */
- function convertMatrix(matrix) {
+function convertMatrix(matrix) {
     matrix = copyMatrix(matrix); // create copy
 
     let lead = 0; // pivot entry
 
     const rowCount = matrix.length
     const colCount = matrix[0].length
-    
+
     for (let r = 0; r < rowCount; r++) {
         // lead exceeds bounds of nested array
         if (lead >= colCount) {
@@ -26,7 +23,6 @@ const copyMatrix = require("../utils/CopyMatrix.js");
         // when leading cell in this row is a zero
         if (matrix[i][lead] === 0) {
             let res = dealWithZeroLead(matrix, i, r, lead, rowCount, colCount);
-            
             // exceeded bounds of matrix
             if (res[0] === - 1) {
                 return matrix;
@@ -37,15 +33,17 @@ const copyMatrix = require("../utils/CopyMatrix.js");
         matrix = swapRows(i, r, matrix);
 
         let leadingVal = matrix[r][lead];
+
         if (leadingVal !== 0) {
             // divide row r by this value
-            matrix[r] = matrix[r].map( el => el / leadingVal);
+            matrix[r] = matrix[r].map(el => el / leadingVal);
         }
+
 
         for (let i = 0; i < rowCount; i++) {
             leadingVal = matrix[i][lead];
             if (i !== r) {
-                let scaledR = matrix[r].map( el => el * leadingVal);
+                let scaledR = matrix[r].map(el => el * leadingVal);
 
                 let rowI = matrix[i];
                 // subtract the scaled row r from row i 
@@ -56,13 +54,23 @@ const copyMatrix = require("../utils/CopyMatrix.js");
         }
         lead++;
     }
-    return matrix;
- }
+    return removeNegativeZero(matrix);
+}
 
- function dealWithZeroLead(matrix, i, r, lead, rowCount, colCount) {
-     while (matrix[i][lead] === 0) {
+function removeNegativeZero(matrix) {
+    let out = []
+
+    matrix.forEach(row => out
+        .push(row
+            .map(el => el === -0 ? 0 : el)))
+
+    return out;
+}
+
+function dealWithZeroLead(matrix, i, r, lead, rowCount, colCount) {
+    while (matrix[i][lead] === 0) {
         i++;
-        
+
         // i has exceeded bounds of matrix
         if (i === rowCount) {
             i = r;
@@ -75,11 +83,11 @@ const copyMatrix = require("../utils/CopyMatrix.js");
                 return [-1, -1];
             }
         }
-     }
+    }
     return [i, lead];
- }
+}
 
- function swapRows(i, r, matrix){
+function swapRows(i, r, matrix) {
     matrix = copyMatrix(matrix); // create copy
 
     let temp = matrix[i];
@@ -87,13 +95,6 @@ const copyMatrix = require("../utils/CopyMatrix.js");
     matrix[r] = temp;
 
     return matrix;
- }
- 
+}
 
-let matrix1 = [[1,2,-1,-4],[2,3,-1,-11],[-2,0,-3,22]]
-let matrix2 = [[1,3,1,9],[1,1,-1,1],[3,11,5,35]];
-let res = convertMatrix(matrix2);
-
-let expected2 = [[ 1, 0, -2, -3 ], [ -0, 1, 1, 4 ], [ 0, 0, 0, 0 ]];
-
-console.log(res);
+export default convertMatrix;
